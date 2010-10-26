a44d2047464529e69c33f1026f9e062ac78049f8
Christopher Joel <chris@cloudflare.com>
2010-10-25 17:07:40 -0700

    Added a 'clone' method that allows for cloning objects and arrays.

--- elementary.js
+++ elementary.js
@@ -1,33 +1,49 @@
 (function (context) {
     
-    context.e = context.e || {
+    var extend = function () {
         
-        extend: function () {
+        var options = arguments,
+            target = options[0] || {}, 
+            other, 
+            property,
+            i;
+        
+        for (i = 1; i < options.length; i++) {
             
-            var options = arguments,
-                target = options[0] || {}, 
-                other, 
-                property,
-                i;
-            
-            for (i = 1; i < options.length; i++) {
+            if ((other = options[i]) != null) {
                 
-                if ((other = options[i]) != null) {
+                for (property in other) {
                     
-                    for (property in other) {
+                    var targetProperty = target[property],
+                        otherProperty = other[property];
+                    
+                    if (otherProperty !== undefined && targetProperty !== otherProperty) {
                         
-                        var targetProperty = target[property],
-                            otherProperty = other[property];
-                        
-                        if (otherProperty !== undefined && targetProperty !== otherProperty) {
-                            
-                            target[property] = otherProperty;
-                        }
+                        target[property] = otherProperty;
                     }
                 }
             }
-            
-            return target;
         }
+        
+        return target;
+    };
+    
+    var clone = function (value) {
+        
+        if (typeof value == 'object') {
+            
+            if (valuet.length)
+                return value.concat();
+            else
+                return e.extend({}, value);
+        }
+        
+        return value;
+    };
+    
+    context.e = context.e || {
+        
+        extend: extend,
+        clone: clone
     };
 })(this);
